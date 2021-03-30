d3e2059f845df30d628ba318aecd7204d6a80c6b
Cheng <z5172720@ad.unsw.edu.au>
2021-03-30 17:44:54 +1100

    remove pio project

---
+++ MiddleStation/MiddleStation-Zephyr/core/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <errno.h>
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <sys/byteorder.h>
+#include "ble_comms.h"
+
+#endif
