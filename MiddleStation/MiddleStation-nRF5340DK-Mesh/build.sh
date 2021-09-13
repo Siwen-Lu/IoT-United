d441e844198ae2f46d1cf480194aeb2033d0a993
swl02 <swl02@github.com>
2021-09-13 05:42:33 -0700

    fixed merging files

---
+++ MiddleStation/MiddleStation-nRF5340DK-Mesh/build.sh
@@ -0,0 +1,2 @@
+west build -b nrf5340dk_nrf5340_cpuapp -p
+west flash
--- MiddleStation/MiddleStation-nRF5340DK-Mesh/src/main.c
+++ MiddleStation/MiddleStation-nRF5340DK-Mesh/src/main.c
@@ -14,13 +14,51 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(chat, CONFIG_LOG_DEFAULT_LEVEL);
 
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/mesh/models.h>
+#include <bluetooth/mesh/dk_prov.h>
+#include "model_handler.h"
+
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+	
+	dk_leds_init();
+	dk_buttons_init(NULL);
+
+	err = bt_mesh_init(bt_mesh_dk_prov_init(), model_handler_init());
+	if (err) {
+		printk("Initializing mesh failed (err %d)\n", err);
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	/* This will be a no-op if settings_load() loaded provisioning info */
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+
+	printk("Mesh initialized\n");
+}
+
 #include "eth_comms.h"
 
 void main(void)
 {
 
 	printk("Initializing...\n");
-	
+
+	int err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
+
 	eth_init();
 	
 	server_init();
