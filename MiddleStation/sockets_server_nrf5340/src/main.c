fe8ba5edf06f86f6739ec4c2fbad6be4976e76d7
swl02 <swl02@github.com>
2021-09-13 04:15:48 -0700

    changing structure

--- MiddleStation/sockets_server_nrf5340/src/main.c
+++ MiddleStation/sockets_server_nrf5340/src/main.c
@@ -1,7 +1,41 @@
 #include "eth_comms.h"
 #include <nrfx_clock.h>
 #include <logging/log.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/mesh/models.h>
+#include <bluetooth/mesh/dk_prov.h>
+#include <dk_buttons_and_leds.h>
+#include "model_handler.h"
+
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+	
+	dk_leds_init();
+	dk_buttons_init(NULL);
+
+	err = bt_mesh_init(bt_mesh_dk_prov_init(), model_handler_init());
+	if (err) {
+		printk("Initializing mesh failed (err %d)\n", err);
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	/* This will be a no-op if settings_load() loaded provisioning info */
+	bt_mesh_prov_enable(BT_MESH_PROV_ADV | BT_MESH_PROV_GATT);
+
+	printk("Mesh initialized\n");
+}
+
 void main(void)
 {	
 	// force the app core running at 128mhz
